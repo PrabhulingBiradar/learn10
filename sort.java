8b0e95224bb532eaf3e6acaa315929ff3570ce5c
PrabhulingBiradar <prabulingabiradar537.xworkz@gmail.com>
2021-12-21 11:14:51 +0530

    sortjava

---
+++ sort.java
@@ -0,0 +1,17 @@
+class Sort{
+static short[] rollnum={56,52,25,54,66,67,68,12,11,10};
+
+public static void main(String[] args){
+
+System.out.println(rollnum[0]);
+System.out.println(rollnum[1]);
+System.out.println(rollnum[2]);
+System.out.println(rollnum[3]);
+System.out.println(rollnum[4]);
+System.out.println(rollnum[5]);
+System.out.println(rollnum[6]);
+System.out.println(rollnum[7]);
+System.out.println(rollnum[8]);
+System.out.println(rollnum[9]);
+}
+}
